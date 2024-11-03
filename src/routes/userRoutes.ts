3c889c3d4383b0b5bc74a89d3dfbe2341c2305c2
f3gomes <fgomesdeluna@gmail.com>
2024-11-03 11:18:00 -0300

    fix: routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -14,6 +14,6 @@ userRoutes.get("/users", getUsers);
 userRoutes.get("/users/names", getUsersNameList);
 userRoutes.get("/users/:email", getUserByEmail);
 
-userRoutes.post("users/new", createUser);
-userRoutes.put("users/:id", updateUser);
-userRoutes.delete("users/:id", deleteUser);
+userRoutes.post("/users/new", createUser);
+userRoutes.put("/users/:id", updateUser);
+userRoutes.delete("/users/:id", deleteUser);
